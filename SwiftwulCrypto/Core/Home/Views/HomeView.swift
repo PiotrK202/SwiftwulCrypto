15cc1def3a6038ce19adcc50b49527878ceda721
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-20 16:23:45 +0100

    added some groups and button View

--- SwiftwulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftwulCrypto/Core/Home/Views/HomeView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
+        ZStack {
         //background layer
         Color.theme.background
             .ignoresSafeArea()
+        
+        // content layer
+            VStack {
+        Text("something")
+        Spacer(minLength: 0)
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .navigationBarHidden(true)
     }
 }
