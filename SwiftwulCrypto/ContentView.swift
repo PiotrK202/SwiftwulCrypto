8b80533f5f1f1ba801435d9a30c4edbb9fafe571
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-20 15:19:02 +0100

    Initial Commit

---
+++ SwiftwulCrypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftwulCrypto
+//
+//  Created by piotr koscielny on 20/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftwulCrypto/SwiftwulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftwulCryptoApp.swift
+//  SwiftwulCrypto
+//
+//  Created by piotr koscielny on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftwulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
