cd9dbbe00208ff8007dc07230fae4e718f9a7e38
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-21 11:46:44 +0100

    new var and func in double extension for 2 decimal points

--- SwiftwulCrypto/Extensions/Double.swift
+++ SwiftwulCrypto/Extensions/Double.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 extension Double {
+    
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    // Convets a double into a currency as a string with 2 decimal places
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
     // Convets a double into a currency with 2-6 decimal places
     private var currencyFormatter: NumberFormatter {
         let formatter = NumberFormatter()
