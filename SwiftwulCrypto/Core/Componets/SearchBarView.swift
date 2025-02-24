f715722d7215576bcdccbdb3472f16f13e599881
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-24 11:34:01 +0100

    created SearchBarView

---
+++ SwiftwulCrypto/Core/Componets/SearchBarView.swift
@@ -0,0 +1,48 @@
+//
+//  SearchBarView.swift
+//  SwiftwulCrypto
+//
+//  Created by piotr koscielny on 24/02/2025.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(Color.theme.secondaryText)
+            
+            TextField("search by name or symbol", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .overlay(
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(Color.theme.accent)
+                    .opacity(searchText.isEmpty ? 0 : 1)
+                    .onTapGesture {
+                        searchText = ""
+                    }
+                
+                ,alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.theme.background)
+            .shadow(color: Color.theme.accent.opacity(0.15), radius: 10, x: 0, y: 0)
+        )
+        .padding()
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(searchText: .constant("a"))
+            .preferredColorScheme(.dark)
+    }
+}
