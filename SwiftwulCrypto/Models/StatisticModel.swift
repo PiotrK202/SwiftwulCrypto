a14249785b473b2475345c7a1448407c20382255
PiotrK202 <piotr.koscielny69@gmail.com>
2025-02-24 13:33:43 +0100

    created statistic Model

---
+++ SwiftwulCrypto/Models/StatisticModel.swift
@@ -0,0 +1,22 @@
+//
+//  StatisticModel.swift
+//  SwiftwulCrypto
+//
+//  Created by piotr koscielny on 24/02/2025.
+//
+
+import Foundation
+
+struct StatisticModel: Identifiable {
+    
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+}
